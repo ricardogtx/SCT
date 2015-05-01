113f642765c8b098cd5b01bd314cb833984d64ea
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-05-01 00:55:32 -0300

    Adicionando imagens para ilustração da página
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- db/migrate/20150430233739_create_drugs.rb
+++ db/migrate/20150430233739_create_drugs.rb
@@ -2,11 +2,12 @@ class CreateDrugs < ActiveRecord::Migration
   def change
     create_table :drugs do |t|
       t.string :name
-      t.string :type
+      t.string :drug_type
       t.string :origin
       t.string :average_ingest
       t.string :ingestion_form
       t.string :effect_average
+      t.string :duration
       t.string :effect_high
       t.string :dependence_psychological
       t.string :dependence_phisical
