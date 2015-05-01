1690d0795d4a8ae827d6df6a562bd79818212d40
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-05-01 01:00:19 -0300

    Criando validações para a Model Drug
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
    Signed-off-by: Flavio Costa <skingk.f@gmail.com>

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,2 +1,6 @@
 class Drug < ActiveRecord::Base
+  validates :name, :drug_type, :origin, :average_ingest, :ingestion_form, :effect_average, :effect_high,
+            :duration, :dependence_psychological, :dependence_phisical, :tolerance, :high_term,
+            :long_effects, :medical_use,
+            :presence => true
 end
