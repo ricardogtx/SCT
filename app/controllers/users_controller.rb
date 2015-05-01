8599b0b01703aa354e0bf9415058cd8f3da34243
LucasAmoedo <lucas_advc@outlook.com>
2015-05-01 18:14:39 -0300

    Implemented method create in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		redirect_to @user, notice: 'Usuario cadastrado com sucesso'
+  	else
+  		render action: "new"
+  	end
+  end
+
 end
