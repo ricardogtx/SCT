14c0672296266a46c1ec872fb9e61d9c7d23e438
Vitor Nere <vitor.nere@hotmail.com>
2015-05-01 17:32:25 -0300

    Implemented attributes and validations of User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+  
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+
+  validates :name,    :presence   =>  true,
+            :lenght               =>  { :maximum => 50 }
+  
+  validates :email,   :presence   =>  true,
+            :format               =>  { :with => email_regex },
+            :uniqueness           =>  { :case_sensitive => false }
+
+  validates :password, :presence  =>  true,
+            :confirmation         =>  true,
+            :lenght               =>  { :within => 6..40 }
+
 end
