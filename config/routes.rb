2de3af900b994d876288ea50eb5b546cbcb73580
Flavio Costa <Skingk.f@gmail.com>
2015-05-01 10:14:48 -0300

    corrigindo e adicionando rotas para home e drogas
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ end
  root 'home#index'
 
  resources :drugs
+ resources :home
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
