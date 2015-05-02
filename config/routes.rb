d02afaec588926a611f2f0961b491bd8b13965bc
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-05-02 16:16:19 -0300

    create default layout for aplication home and add some routes
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
  resources :clinics do
    collection {post :import}
 end
- root 'clinics#index'
+ root 'home#index'
+
+ resources :drugs
+ resources :home
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
