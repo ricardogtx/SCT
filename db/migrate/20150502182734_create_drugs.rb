9f87e59775194ba36a422ccd10c98ec80b9b54b9
Flavio Costa <Skingk.f@gmail.com>
2015-05-02 15:36:36 -0300

    create drugs database table
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>
    Signed-off-by: Kleber Brito <kleberbritomoreira10@gmail.com>

--- db/migrate/20150502182734_create_drugs.rb
+++ db/migrate/20150502182734_create_drugs.rb
@@ -1,7 +1,19 @@
 class CreateDrugs < ActiveRecord::Migration
   def change
     create_table :drugs do |t|
-
+      t.string :name
+      t.string :drug_type
+      t.string :origin
+      t.string :average_ingest
+      t.string :ingestion_form
+      t.string :effect_average
+      t.string :duration
+      t.string :effect_high
+      t.string :dependence_psychological
+      t.string :dependence_phisical
+      t.string :tolerance
+      t.string :long_effects
+      t.string :medical_use
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    dataDrug = YAML::load_file(File.join(Rails.root,"public/static/drugs.yml"))
+    dataDrug.each do |data|
+
+    drug = Drug.new
+
+    drug.name = data["Nome"]
+    drug.drug_type = data["Tipo_droga"]
+    drug.origin = data["Origem"]
+    drug.average_ingest = data["Quantidade_media_ingerida"]
+    drug.ingestion_form = data["Forma_ingestao"]
+    drug.effect_average = data["Efeitos_a_curto_prazo_quantidade_media"]
+    drug.duration = data["Duracao"]
+    drug.effect_high = data["Efeitos_a_curto_prazo_grandes_quantidades"]
+    drug.dependence_psychological = data["Risco_de_dependencia_psicologica"]
+    drug.dependence_phisical = data["Risco_de_dependencia_fisica"]
+    drug.tolerance = data["Tolerancia"]
+    drug.long_effects = data["Efeitos_a_longo_prazo"]
+    drug.medical_use = data["Utilizacao_medica"]
+    drug.save!
+
+    end
