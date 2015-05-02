d2f46e7a69290603ac4e4003c4a621a4b41c8781
LucasAmoedo <lucas_advc@outlook.com>
2015-05-01 23:35:38 -0300

    Implemented method create in SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,3 +2,22 @@ class SessionController < ApplicationController
 	def new
 	end
 
+	def create 
+		user = User.authenticate (params[:session][:email], params[:session][:password])
+
+		if user.nil?
+			flash.now[:error] = "Senha/Email invalidos."
+			render :new
+		else
+			sign_in user
+			redirect_to user
+		end
+
+	end
+
+	def destroy
+		sign_out
+		redirect_to signin_path
+	end
+
+end
