6e051dba2f7c3a274ec949ae152694be2bf835f9
LucasAmoedo <lucas_advc@outlook.com>
2015-05-02 00:07:05 -0300

    Implemented current user and sign_out methods in SessionsHelper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,4 +12,17 @@ module SessionsHelper
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
+
+	def signed_in?
+		current_user.nil?
+	end
+
+	def sign_out
+		session[:user_id] = nil
+		self.current_user = nil
+	end
+
+	def current_user?(user)
+		user == current_user
+	end
 	
