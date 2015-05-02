b3748f625f40f50a3ae4032c6e96d9573c9e847e
Flavio Costa <Skingk.f@gmail.com>
2015-05-02 15:54:05 -0300

    create validates for drug model
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>
    Signed-off-by: Kleber Brito <kleberbritomoreira10@gmail.com>

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,2 +1,5 @@
 class Drug < ActiveRecord::Base
+      validates :name, :drug_type, :origin, :average_ingest, :ingestion_form, :effect_average, :duration, :effect_high,
+            :dependence_psychological, :dependence_phisical, :tolerance, :medical_use,
+            :presence => true
 end
