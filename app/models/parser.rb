8d6a32d2bf708d8d874ea555a89b8e884360ce4d
ricardogtx <ricardocdzg@hotmail.com>
2015-05-17 23:13:23 -0300

    Fix problems of unicode

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,7 +1,7 @@
 class Parser < ActiveRecord::Base
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true, encoding: "iso-8859-1:UTF-8") do |row|
+    CSV.foreach(file.path, headers: true, encoding: "UTF-8") do |row|
       Clinic.create! row.to_hash
     end
   end
