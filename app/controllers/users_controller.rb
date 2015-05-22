ab7299610387f649f8a72669523cdf5d35ecbe09
Vitor Nere <vitor.nere@hotmail.com>
2015-05-21 21:47:40 -0300

    Added 'before action' to reduce code size

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate, only: [:index, :login]
+  before_action :user_need_to_be_logged, only: [:show, :profile, :clinic_profile, :edit, :update, :clinic]
 
   attr_accessor :password
 
@@ -20,15 +22,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    return authenticate if is_user_logged?
   end
 
   def login
-    authenticate if is_user_logged?
-
     if !params[:do_login].nil? && request.post?
       user = User.find_by_email(params[:do_login][:email])
-      
+
       if user.nil?
         flash.now[:error] = "Insira um email e senha."
         render :login
@@ -51,7 +50,7 @@ class UsersController < ApplicationController
   def logout
     user_was_logged = is_user_logged?
     session[:user_id] = nil
-    
+
     if request.xhr?
       render :json => {user_was_logged: user_was_logged}
     else
@@ -65,24 +64,18 @@ class UsersController < ApplicationController
 
   def profile
     @user = current_user
-    if session[:user_id].nil?
-      redirect_to :users, notice: 'Voce precisa fazer login!'
-    end
+
     redirect_to :users_clinic_profile unless @user.clinic.nil?
   end
 
   def clinic_profile
     @user = current_user
-    if session[:user_id].nil?
-      redirect_to :users, notice: 'Voce precisa fazer login!'
-    end
+
     redirect_to :users_profile unless !@user.clinic.nil?
   end
 
   def edit
     @user = current_user
-
-    redirect_to :users, notice: 'Voce precisa fazer login!' unless is_user_logged?
   end
 
   def update
@@ -99,13 +92,9 @@ class UsersController < ApplicationController
 
   def clinic
     @user = current_user
-    if session[:user_id].nil?
-      redirect_to :users, notice: 'Voce precisa fazer login!'
-    end
   end
 
   def clinic_edit
-    redirect_to :users, notice: 'Voce precisa fazer login!' unless is_user_logged?
     @user = current_user
     redirect_to :users_profile_edit if @user.clinic.nil?
   end
@@ -122,5 +111,25 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def authenticate
+    if is_user_logged?
+      user = current_user
+
+      if user.authenticate_admin_user(user)
+        redirect_to :admin, :notice => "Logado!"
+      elsif user.authenticate_clinic_user(user)
+        redirect_to :users_clinic, :notice => "Logado!"
+      else
+        redirect_to :users, :notice => "Logado!"
+      end
+    end
+  end
+
+  def user_need_to_be_logged
+    if session[:user_id].nil?
+      redirect_to :users, notice: 'Voce precisa fazer login!'
+    end
   end
 end
