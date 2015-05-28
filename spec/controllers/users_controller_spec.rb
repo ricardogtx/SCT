793a7b6982d8c65191341833d0625a134e8df68f
Vitor Nere <vitor.nere@hotmail.com>
2015-05-28 10:27:01 -0300

    Tested Login of UsersController

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe UsersController, type: :controller do
   before :all do
     User.destroy_all
     User.create! :name=>"vitor", :email=>"vitor.nere@hotmail.com", :password=>"123456", :password_confirmation=>"123456"
-    Clinic.create! :nome=>"rspec", :tipo_logradouro=>"rspec", :nome_logradouro=>"rspec", :numero_logradouro=>"rspec", 
-                   :complemento=>"rspec", :bairro=>"rspec", :cep=>"rspec", :estado=>"rspec", :municipio=>"rspec", :latitude=>"rspec", 
-                   :longitude=>"rspec", :telefone_1=>"rspec", :telefone_2=>"rspec", :e_mail=>"rspec", :publico_atendido=>"rspec", 
+    Clinic.create! :nome=>"rspec", :tipo_logradouro=>"rspec", :nome_logradouro=>"rspec", :numero_logradouro=>"rspec",
+                   :complemento=>"rspec", :bairro=>"rspec", :cep=>"rspec", :estado=>"rspec", :municipio=>"rspec", :latitude=>"rspec",
+                   :longitude=>"rspec", :telefone_1=>"rspec", :telefone_2=>"rspec", :e_mail=>"rspec", :publico_atendido=>"rspec",
                    :grupo_especifico=>"rspec", :qual_grupo_especifico=>"rspec", :sexo_do_publico=>"rspec", :modalidade=>"rspec"
   end
 
@@ -63,12 +63,37 @@ RSpec.describe UsersController, type: :controller do
     end
 
     it "Should render users/clinic if user logged is associated to a clinic" do
-      session[:user_id] = User.last.id
       User.last.clinic = Clinic.last
+      session[:user_id] = User.last.id
 
       get :index
 
-      expect(response).to redirect_to(:users_clinic)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "GET #login" do
+    it "Should render users/login if user has request get" do
+      session[:user_id] = nil
+      get :login
+      expect(response).to render_template(:login)
+    end
+
+    it "Should show flash if user is missing or wrong email" do
+      post :login, :do_login => { :email => "email@hotmail.com", :password => "123456" }
+      expect(response).to render_template(:login)
+      expect(flash[:error]).to be_present
+    end
+
+    it "Should redirect to users if user gets login" do
+      post :login, :do_login => { :email => "vitor.nere@hotmail.com", :password => "123456" }
+      expect(response).to redirect_to(:users)
+    end
+
+    it "Should show flash if user input a wrong password" do
+      post :login, :do_login => { :email => "vitor.nere@hotmail.com", :password => "1234569283" }
+      expect(response).to render_template(:login)
+      expect(flash[:error]).to be_present
     end
   end
 end
