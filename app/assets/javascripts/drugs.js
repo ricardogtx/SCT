00ad8de7bb901e43640ffe3a2799a0eff2df9141
Flavio Costa <Skingk.f@gmail.com>
2015-05-29 18:20:39 -0300

    Add the new javascript to googlechart graphs
    Signed-off-by: Flavio Costa <Skingk.f@gmail.com>

---
+++ app/assets/javascripts/drugs.js
@@ -0,0 +1,40 @@
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+        var drugs_data = document.getElementsByName("drugs[]");
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+
+        var drug_list = [];
+        for(var i=0, len=drugs_data.length; i < len; i++) {
+          var drug = drugs_data[i].value.split('|');
+
+          drug_list.push([
+            drug[0], parseFloat(drug[1])
+          ]);
+        }
+
+
+        data.addRows(drug_list);
+
+        // Set chart options
+        var options = {'title':'O uso na vida de drogas de abuso na população brasileira',
+                       'width':600,
+                       'height':400};
+
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
