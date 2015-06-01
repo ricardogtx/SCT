d89f10c186e73416ff3f69c0d972534c839f1eb5
Leonardo <leomeister22@gmail.com>
2015-06-01 18:28:08 -0300

    Data bank can no longer be duplicated

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,5 +1,8 @@
 class Drug < ActiveRecord::Base
-      validates :name, :drug_type, :origin, :average_ingest, :ingestion_form, :effect_average, :duration, :effect_high,
+      validates :drug_type, :origin, :average_ingest, :ingestion_form, :effect_average, :duration, :effect_high,
             :dependence_psychological, :dependence_phisical, :tolerance, :medical_use,
             :presence => true
+      validates :name,
+      		:presence => true,
+            :uniqueness => true
 end
