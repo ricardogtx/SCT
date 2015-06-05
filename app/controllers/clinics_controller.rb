9804593e621d0f101b5890bd1b85c002fc132b44
Vitor Nere <vitor.nere@hotmail.com>
2015-06-05 12:47:48 -0300

    Made redirection of the general maps to the clinics by city

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,5 +1,6 @@
 class ClinicsController < ApplicationController
   before_action :set_clinic, only: [:show, :edit, :update, :destroy]
+  skip_before_filter :verify_authenticity_token, :only => [:id_of_city]
 
   def index
     @search = Clinic.search(params[:q])
@@ -23,6 +24,14 @@ class ClinicsController < ApplicationController
     end
   end
 
+  def id_of_city
+    state = State.find_by_uf(params[:uf])
+
+    if request.xhr?
+      render :json => {city_id: state.id}
+    end
+  end
+
 private
   def set_clinic
     @clinics = Clinic.find(params[:id])
@@ -31,10 +40,7 @@ private
   def clinic_params
     params.require(:clinic, :state).permit(:nome, :tipo_logradouro, :nome_logradouro, :numero_logradouro, :complemento, :bairro, :cep, :state_id, :municipio,
                                    :latitude, :longitude, :telefone_1, :telefone_2, :e_mail, :publico_atendido, :grupo_especifico,
-                                   :qual_grupo_especifico, :sexo_do_publico, :modalidade, :name)
-  end
-
-  def general_map
+                                   :qual_grupo_especifico, :sexo_do_publico, :modalidade, :name, :uf)
   end
 
 end
