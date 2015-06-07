c4615faab686da99b14111b83f9b4af5b073659a
Leonardo <leomeister22@gmail.com>
2015-06-07 13:41:24 -0300

    Created campaign model and basic controller functions

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,4 +1,20 @@
 class CampaignsController < ApplicationController
-  def index
-  end
+
+	def new
+		@campaign = Campaign.new
+	end
+
+	def index
+  		@campaign = Campaign.all
+	end
+
+	def create
+		@campaign = Campaign.new(campaign_params)
+		if @campaign.save
+			redirect_to campanhas_path, notice: "Enviado com sucesso."
+		end
+	end
+
+	def destroy
+	end
 end
