c3ffb5382b96cf17ad0f9ec1aece1ac38b27b72a
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-06-08 22:24:17 -0300

    adjusting update controller and seed to CAD
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,7 @@
     campaign.name = "Cartilha sobre tabaco"
     campaign.url = "http://www.obid.senad.gov.br/portais/OBID/biblioteca/documentos/Publicacoes/cartilhas/Sem_Logo/329743.pdf"
     campaign.description = "Cartilha sobre o tabaco e seus riscos."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/cartilha-sobre-o-tabaco-1-728.jpg"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
@@ -47,6 +48,7 @@
     campaign.name = "Cartilha para pais de crianças"
     campaign.url = "http://www.obid.senad.gov.br/portais/OBID/biblioteca/documentos/Publicacoes/cartilhas/Sem_Logo/329742.pdf"
     campaign.description = "Cartilha de auxilio aos pais sobre educação infantil."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/drogas-cartilha-para-pais-de-crianas-1-728.jpg"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
@@ -56,6 +58,7 @@
     campaign.name = "Cartilha sobre álcool e jovens"
     campaign.url = "http://www.obid.senad.gov.br/portais/OBID/biblioteca/documentos/Publicacoes/cartilhas/Sem_Logo/329744.pdf"
     campaign.description = "Cartilha educacional sobre o combate ao álcool."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/cartilha-alcool-e-jovens-1-728.jpg"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
@@ -65,6 +68,7 @@
     campaign.name = "Cartilha mudando comportamentos"
     campaign.url = "http://www.obid.senad.gov.br/portais/OBID/biblioteca/documentos/Publicacoes/cartilhas/Sem_Logo/329740.pdf"
     campaign.description = "Cartilha de auxilio à mudança de hábito."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/cartilha-mudando-comportamentosweb-1-728.jpg"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
@@ -74,6 +78,7 @@
     campaign.name = "Cartilha sobre maconha, cocaína e inalantes"
     campaign.url = "http://www.obid.senad.gov.br/portais/OBID/biblioteca/documentos/Publicacoes/cartilhas/Sem_Logo/329738.pdf"
     campaign.description = "Cartilha educacional sobre os perigos de diversas drogas."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/cartilha-maconha-cocainainalantes-1-728.jpg"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
@@ -83,6 +88,7 @@
     campaign.name = "Cartilha para educadores"
     campaign.url = "http://www.obid.senad.gov.br/portais/OBID/biblioteca/documentos/Publicacoes/cartilhas/Sem_Logo/329739.pdf"
     campaign.description = "Cartilha de auxílio a educadores contra as drogas."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/drogas-cartilha-para-educadores-1-728.jpg"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
@@ -92,6 +98,7 @@
     campaign.name = "Cartilha para pais de adolescentes"
     campaign.url = "http://www.obid.senad.gov.br"
     campaign.description = "Cartilha de auxilio aos pais sobre como lidar com a questão das drogas com adolescentes."
+    campaign.avatar = File.open(File.join(Rails.root, "app/assets/images/paisdeadolescentes.png"), "r")
 
     puts "="*80, "Saving: #{campaign.name}", "="*80
     campaign.save!
