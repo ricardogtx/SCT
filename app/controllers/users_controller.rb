f2926a91be460bf7a02c55f27111e9aa22fb30e6
Vitor Nere <vitor.nere@hotmail.com>
2015-06-12 22:33:58 -0300

    Created the logic for user authentication and added field in the register for user request clinic association

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :user_need_to_be_logged, only: [:show, :profile, :edit, :update, :admin, :clinic_edit]
   before_action :user_need_associated_a_clinic_or_admin, only: [:index, :profile, :edit, :update, :admin, :clinic_edit]
+  before_action :user_need_is_approved_by_admin, only: [:index, :profile, :edit, :update, :admin, :clinic_edit]
+
 
   attr_accessor :password
 
@@ -29,7 +31,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @user = current_userd
+    @user = current_user
     if request.post? && !params[:do_login][:email].blank? && !params[:do_login][:password].blank?
       user = User.find_by_email(params[:do_login][:email])
 
@@ -85,16 +87,16 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :solicit_association)
   end
 
   def authenticate
     if is_user_logged?
       user = current_user
 
-      if user.authenticate_admin_user(user)
+      if user.authenticate_admin_user?(user)
         redirect_to :admin, :notice => "Logado!"
-      elsif user.authenticate_clinic_user(user)
+      elsif user.authenticate_clinic_user?(user)
         redirect_to :users, :notice => "Logado!"
       else
         redirect_to :logout, notice: 'Voce precisa esta associado a uma clínica!'
@@ -106,8 +108,18 @@ class UsersController < ApplicationController
     user = current_user
 
     if user
-      unless user.authenticate_clinic_user(user) or user.authenticate_admin_user(user)
+      unless user.authenticate_clinic_user?(user) or user.authenticate_admin_user?(user)
         redirect_to :logout, notice: 'Voce precisa esta associado a uma clínica!'
+      end
+    end
+  end
+
+  def user_need_is_approved_by_admin
+    user = current_user
+
+    if user
+      unless user.authenticate_user?(user)
+        redirect_to :logout
       end
     end
   end
