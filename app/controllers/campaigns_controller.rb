019264b375b4f682856a131e77efbc42d3fd2015
Vitor Nere <vitor.nere@hotmail.com>
2015-06-15 23:39:32 -0300

    Created Admin autheticate in CAD and testimonials only edit before authenticate

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,44 +1,45 @@
 class CampaignsController < ApplicationController
 
-  def new
-    @campaign = Campaign.new
-  end
+	def new
+		@campaign = Campaign.new
+	end
 
-  def create
-    @campaign = Campaign.new(campaign_params)
-    if @campaign.save
-      redirect_to campanhas_path, notice: "Criado com sucesso!"
-    end
-  end
+	def create
+		@campaign = Campaign.new(campaign_params)
+		if @campaign.save
+			redirect_to campanhas_path, notice: "Criado com sucesso!"
+		end
+	end
 
-  def show
-      @campaign = Campaign.find(params[:id])
-  end
+	def show
+    	@campaign = Campaign.find(params[:id])
+	end
 
-  def index
-      @campaigns = Campaign.all
-  end
+	def index
+      @user = current_user
+  		@campaigns = Campaign.all
+	end
 
-  def destroy
-      @campaign = Campaign.find(params[:id]).destroy
-      redirect_to campanhas_path, notice: "Campanha deletada."
-  end
+	def destroy
+    	@campaign = Campaign.find(params[:id]).destroy
+    	redirect_to campanhas_path, notice: "Campanha deletada."
+	end
 
-  def edit
-      @campaign = Campaign.find params[:id]
+	def edit
+    @campaign = Campaign.find params[:id]
 
       redirect_to "/campaigns" unless @campaign
     end
 
-    def update
-      @campaign = Campaign.find params[:id]
+  def update
+    @campaign = Campaign.find params[:id]
 
-      if @campaign.update_attributes(campaign_params)
-        redirect_to campaigns_path, :notice => "Campanha foi atualizada"
-      else
-        render "edit"
-      end
+    if @campaign.update_attributes(campaign_params)
+      redirect_to campaigns_path, :notice => "Campanha foi atualizada"
+    else
+      render "edit"
     end
+  end
 
   private
   def campaign_params
