a7dbb593dc319ae7e41cf997918109cbfd120111
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-06-19 20:31:43 -0300

    Creation controllers for CRUD in drugs
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -6,4 +6,40 @@ class DrugsController < ApplicationController
   def show
     @drug = Drug.find params[:id]
   end
+
+  def new
+    @drug = Drug.new
+  end	
+
+  def create
+    @drug = Drug.new(drugs_params)
+    
+    if @drug.save
+	  redirect_to drugs_path, notice: "Criada com sucesso!"
+	end  	
+  end
+
+  def edit
+    @drug = Drug.find params[:id]
+  end
+
+  def destroy
+    @drug = Drug.find(params[:id]).destroy
+    redirect_to drugs_path, notice: "Droga deletada."	
+  end
+
+  def update
+   @drug = Drug.find params[:id]
+
+    if @drug.update_attributes(drugs_params)
+      redirect_to drugs_path, :notice => "Droga foi atualizada"
+    else
+      render "edit"
+    end
+  end	
+
+  private
+    def drugs_params
+      params.require(:drug).permit(:name, :drug_type, :dependence_pysichological, :dependence_phisical, :description, :avatar)
+    end	
 end
