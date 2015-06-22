69534a3a27a6a54064f6de177a9b2bbd71ba3991
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-06-22 11:37:34 -0300

    Creation before all for functional drugs test
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- spec/controllers/drugs_controller_spec.rb
+++ spec/controllers/drugs_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe DrugsController, type: :controller do
-  
+	before :all do
+    Drug.create(name: "test drugs", description: "test drugs", drug_type: "test drugs", 
+      origin: "test drugs", average_ingest: "test drugs", ingestion_form: "test drugs", 
+      duration: "test drugs", effect_high: "test drugs", medical_use: "test drugs", 
+      dependence_psychological: "test drugs", tolerance: "test drugs",
+      dependence_phisical: "test drugs", long_effects: "test drugs")
+    Drug.create(name: "test drugs", description: "test drugs", drug_type: "test drugs", 
+      origin: "test drugs", average_ingest: "test drugs", ingestion_form: "test drugs", 
+      duration: "test drugs", effect_high: "test drugs", medical_use: "test drugs", 
+      dependence_psychological: "test drugs", tolerance: "test drugs",
+      dependence_phisical: "test drugs", long_effects: "test drugs")
+    Drug.create(name: "test drugs", description: "test drugs", drug_type: "test drugs", 
+      origin: "test drugs", average_ingest: "test drugs", ingestion_form: "test drugs", 
+      duration: "test drugs", effect_high: "test drugs", medical_use: "test drugs", 
+      dependence_psychological: "test drugs", tolerance: "test drugs",
+      dependence_phisical: "test drugs", long_effects: "test drugs")
+    Drug.create(name: "test drugs", description: "test drugs", drug_type: "test drugs", 
+      origin: "test drugs", average_ingest: "test drugs", ingestion_form: "test drugs", 
+      duration: "test drugs", effect_high: "test drugs", medical_use: "test drugs", 
+      dependence_psychological: "test drugs", tolerance: "test drugs",
+      dependence_phisical: "test drugs", long_effects: "test drugs")
+  end	  
 end
