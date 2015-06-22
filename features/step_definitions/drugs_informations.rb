136d72bb7a2b8fc8ddf828e22074017726d4d4ba
Lucas Amoêdo <lucas_advc@outlook.com>
2015-06-21 23:36:19 -0300

    fixed drugs cucumber tests

--- features/step_definitions/drugs_informations.rb
+++ features/step_definitions/drugs_informations.rb
@@ -5,3 +5,7 @@ end
 Then(/^I am redirected to the drugs page$/) do
   page.should have_title("SCT - Sistemas de Comunidades Terapeuticas")
 end 
+
+When(/^I press the button 'Tipos de Drogas' in the navigation bar$/) do
+  page.find('#drug_index').click
+end
--- features/step_definitions/drugs_step.rb
+++ features/step_definitions/drugs_step.rb
@@ -2,98 +2,15 @@ Given(/^I visit the drugs page$/) do
 	visit '/drogas' 
 end
 
-When(/^I press the button alcool$/) do
-  page.find('#btn_alcohol').click
+Given(/^I have a drug in the database$/) do
+   @drug = Drug.create! :name => "cucumber drug", :description => "cucumber drug", :drug_type => "cucumber drug", :origin => "cucumber drug", :average_ingest => "cucumber drug", :ingestion_form => "cucumber drug", :duration => "cucumber drug", :effect_high => "cucumber drug", :dependence_psychological => "cucumber drug", :dependence_phisical => "cucumber drug", :tolerance => "cucumber drug", :long_effects => "cucumber drug", :medical_use => "cucumber drug"      
 end
 
-Then(/^I am redirected to the Alcohol Saiba Mais page$/) do
-  page.should have_content("Quadro resumo: Álcool")
+When(/^I press the "(.*?)" button of any given drug$/) do |link|
+  click_link(@drug.id)
 end
 
-When(/^I press the button in amphetamines$/) do
-  page.find('#btn_amphetamine').click
+Then(/^I am redirected to that drug information page$/) do
+  expect(current_path).to eq("/drugs/#{@drug.id}")
 end
 
-Then(/^I am redirected to the ampheamines page$/) do
-  page.should have_content("Quadro resumo: Anfetaminas")
-end
-
-When(/^I press the button in hallucinoges$/) do
-  page.find('#btn_hallucinogen').click
-end
-
-Then(/^I am redirected to the hallucinoges page$/) do
-  page.should have_content("Quadro resumo: Alucinógenos")
-end
-
-When(/^I press the button in antidepressants$/) do
-  page.find('#btn_antidepressant').click
-end
-
-Then(/^I am redirected to the antidepressants page$/) do
-  page.should have_content("Quadro resumo: Antidepressivos")
-end
-
-When(/^I press the button in barbiturates$/) do
-  page.find('#btn_barbiturates').click
-end
-
-Then(/^I am redirected to the barbiturates page$/) do
-  page.should have_content("Quadro resumo: Barbitúricos")
-end
-
-When(/^I press the button in caffeine$/) do
-  page.find('#btn_caffeine').click
-end
-
-Then(/^I am redirected to the caffeine page$/) do
-  page.should have_content("Quadro resumo: Cafeína")
-end
-
-When(/^I press the button in cannabis$/) do
-  page.find('#btn_cannabis').click
-end
-
-Then(/^I am redirected to the cannabis page$/) do
-  page.should have_content("Quadro resumo: Cannabis Sativa")
-end
-
-When(/^I press the button in cocaine$/) do
-  page.find('#btn_cocaine').click
-end
-
-Then(/^I am redirected to the cocaine page$/) do
-  page.should have_content("Quadro resumo: Cocaína")
-end
-
-When(/^I press the button in inhalants$/) do
-  page.find('#btn_inhalants').click
-end
-
-Then(/^I am redirected to the inhalants page$/) do
-  page.should have_content("Quadro resumo: Inalantes")
-end
-
-When(/^I press the button in narcotics$/) do
-  page.find('#btn_narcotics').click
-end
-
-Then(/^I am redirected to the narcotics page$/) do
-  page.should have_content("Quadro resumo: Narcóticos")
-end
-
-When(/^I press the button in nicotine$/) do
-  page.find('#btn_nicotine').click
-end
-
-Then(/^I am redirected to the nicotine page$/) do
-  page.should have_content("Quadro resumo: Nicotina")
-end
-
-When(/^I press the button in tranquilizer$/) do
-  page.find('#btn_tranquilizer').click
-end
-
-Then(/^I am redirected to the tranquilizer page$/) do
-  page.should have_content("Quadro resumo: Tranquilizantes")
-end
