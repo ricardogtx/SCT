d9fdb89faa8a1841248e7cabce460ee4dcd88c3d
Kleber Brito Moreira <kleberbritomoreira10@gmail.com>
2015-06-22 11:39:21 -0300

    Creation test for controller index of drugs
    Signed-off-by: Kleber Brito Moreira <kleberbritomoreira10@gmail.com>

--- spec/controllers/drugs_controller_spec.rb
+++ spec/controllers/drugs_controller_spec.rb
@@ -22,5 +22,15 @@ RSpec.describe DrugsController, type: :controller do
       duration: "test drugs", effect_high: "test drugs", medical_use: "test drugs", 
       dependence_psychological: "test drugs", tolerance: "test drugs",
       dependence_phisical: "test drugs", long_effects: "test drugs")
+  end
+
+  describe "GET /drugs => drugs#index" do
+    it "should set @drugs with all drugs" do 
+      all_posts = Drug.all
+
+      get :index
+
+      expect(assigns(:drugs)).to match_array(all_posts)   
+    end  
   end	  
 end
